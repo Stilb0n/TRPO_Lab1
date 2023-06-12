a7f8f4c9380b3d77822879319bbe7091d4916241
Stilbon <stilbon01@gmail.com>
2023-06-12 16:02:44 +0700

    Закончил работать с классом FileEditor
    Реализовал методы: OpenFile, Write, Attach, Detach,Notify.

--- untitled5/fileeditor.cpp
+++ untitled5/fileeditor.cpp
@@ -42,6 +42,56 @@ FileEditor (const FileEditor& other)
     this->list_observer_=other.list_observer_;
 }
 
+void OpenFile(const char* path) {
+    if (existing)
+    file.close();
+    file.open(path);
+    if (file.fail())
+    {
+        existing = 0;
+    }
+    else
+    {
+        file.seekg(0, ios::end);
+        size = file.tellg();
+        existing = 1;
+    }
+    Notify();
+}
+~FileEditor() {
+    if (existing)
+    file.close();
+}
+
+void Write(string data) {
+    if (existing)
+    {
+     file.seekg(0, ios::end);
+        file << data;
+        edited = 1;
+
+        size = file.tellg();
+    }
+    Notify();
+
+}
+void Attach(IObserver* observer) override {
+    list_observer_.push_back(observer);
+
+}
+void Detach(IObserver* observer) override {
+    list_observer_.remove(observer);
+}
+void Notify() override { //!!!!!!!!!!!!!!!!
+    list<IObserver*>::iterator iterator = list_observer_.begin();
+
+    while (iterator != list_observer_.end()) {
+        (*iterator)->Update(size, existing);
+
+        ++iterator;
+    }
+}
+
 private:
  list<IObserver*> list_observer_;
  string message_;
