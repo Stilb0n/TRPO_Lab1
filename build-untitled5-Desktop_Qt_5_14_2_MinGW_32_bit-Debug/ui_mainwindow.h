b4e8011241385a9192595e733a8f3f474916a575
Stilbon <stilbon01@gmail.com>
2023-06-13 13:10:52 +0700

    Реализовал пользовательский интерфейс.
    Добавил две кнопки. Одну для редактирования, другую для проверки на существование. А так же разместил редактор однострочного текста.

---
+++ build-untitled5-Desktop_Qt_5_14_2_MinGW_32_bit-Debug/ui_mainwindow.h
@@ -0,0 +1,84 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.2
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QGroupBox *groupBox;
+    QLineEdit *path;
+    QPushButton *pushButton_2;
+    QPushButton *pushButton;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName(QString::fromUtf8("groupBox"));
+        groupBox->setGeometry(QRect(150, 120, 191, 111));
+        path = new QLineEdit(groupBox);
+        path->setObjectName(QString::fromUtf8("path"));
+        path->setGeometry(QRect(10, 20, 171, 20));
+        pushButton_2 = new QPushButton(groupBox);
+        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
+        pushButton_2->setGeometry(QRect(0, 50, 191, 23));
+        pushButton = new QPushButton(groupBox);
+        pushButton->setObjectName(QString::fromUtf8("pushButton"));
+        pushButton->setGeometry(QRect(10, 80, 161, 24));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "\320\222\320\262\320\265\320\264\320\270\321\202\320\265 \320\277\321\203\321\202\321\214 \321\204\320\260\320\271\320\273\320\260:", nullptr));
+        pushButton_2->setText(QCoreApplication::translate("MainWindow", "\320\237\321\200\320\276\320\262\320\265\321\200\320\272\320\260 \320\275\320\260 \321\201\321\203\321\211\320\265\321\201\321\202\320\276\320\262\320\260\320\275\320\270\320\265", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "\320\224\320\276\320\261\320\260\320\262\320\270\321\202\321\214 \320\262 \321\204\320\260\320\271\320\273 \320\261\321\203\320\272\320\262\321\203 \320\220", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
--- untitled5/mainwindow.cpp
+++ untitled5/mainwindow.cpp
@@ -1,15 +1,50 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "fileeditor.cpp"
+#include <QString>
+#include <QMessageBox>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
 }
-
 MainWindow::~MainWindow()
 {
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+    QString path2 = ui->path->text();
+
+    FileEditor obj1(path2.toStdString());
+    QString OUT;
+    QString EDIT;
+    EditChecker z1;
+    ExsistingChecker x;
+    obj1.Attach(&z1);
+    obj1.Attach(&x);
+    obj1.Write("A");
+        OUT = x.FEx;
+    EDIT = z1.FEd;
+    OUT = OUT + " " + EDIT;
+    QMessageBox::information(this,"Редактирование",OUT);
+
+}
+
+void MainWindow::on_pushButton_2_clicked()
+{
+    QString path2 = ui->path->text();
+
+      FileEditor obj1(path2.toStdString());
+    QString OUT;
+    EditChecker z1;
+    ExsistingChecker x;
+    obj1.Attach(&z1);
+    obj1.Attach(&x);
+    obj1.Write("");
+    OUT = x.FEx;
+    QMessageBox::information(this,"Проверка на существование",OUT);
+}
